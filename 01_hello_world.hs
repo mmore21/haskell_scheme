6c77f15ec74efd0ed7592afac380996328c05163
Mason Moreland <mazc@protonmail.ch>
2020-08-16 14:36:53 -0700

    Finish intro exercises

--- 01_hello_world.hs
+++ 01_hello_world.hs
@@ -3,5 +3,9 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn ("Hello" ++ args !! 0)
+  args <- getArgs
+  putStrLn ("The sum of the two CLI arguments are: " ++ show (read (args !! 0) + read (args !! 1)))
+
+  putStr "Enter name: "
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
